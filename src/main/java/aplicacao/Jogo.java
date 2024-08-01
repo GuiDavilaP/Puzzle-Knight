4de3edcd481e0b83097471e9348f8a421df62a42
bfzawacki <bfzawacki@inf.ufrgs.br>
2024-08-01 00:31:22 -0300

    mais puzzles

--- src/main/java/aplicacao/Jogo.java
+++ src/main/java/aplicacao/Jogo.java
@@ -17,7 +17,7 @@ import static aplicacao.pecas.Cor.*;
 import static java.nio.file.Files.readAllLines;
 
 public class Jogo {
-    private static final int NUMPROB = 5;
+    private static final int NUMPROB = 11;
     //Problema problema;
     private static Peca[][] tabuleiroJogo = new Peca[8][8];
     //private static ArrayList<Jogada> jogadas = new ArrayList<>();
